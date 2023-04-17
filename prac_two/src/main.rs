55629a7c56b9a8473a9093900854f2fd06cd018a
lastrucci01 <u20430168@tuks.co.za>
2023-04-17 08:54:17 +0200

    imports & gitignore

--- prac_two/src/main.rs
+++ prac_two/src/main.rs
@@ -1,3 +1,42 @@
+pub mod ast;
+pub mod lexer;
+pub mod parser;
+pub mod table;
+pub mod token;
+
+use std::{env, fs};
+
+use ast::AST;
+use lexer::Lexer;
+use parser::Parser;
+use token::Token;
+
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = env::args().collect();
+
+    if args.len() < 2 {
+        println!("Usage: {} <file_path>", args[0]);
+        return;
+    }
+
+    let file_path = &args[1];
+    let source_code = match fs::read_to_string(file_path) {
+        Ok(contents) => contents,
+        Err(e) => {
+            println!("Unable to read file: {}", e);
+            return;
+        }
+    };
+
+    // println!("{}", source_code);
+    let lexer = Lexer::new(&source_code);
+    let tokens = lexer.collect::<Vec<Token>>();
+
+    let mut parser = Parser::new(tokens);
+    let tree = parser.parse();
+
+    // dbg!(tree.clone());
+    let mut ast = AST::new(tree);
+    ast.prune();
+    ast.to_xml();
 }
